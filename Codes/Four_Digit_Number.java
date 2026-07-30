dac13b6bc064277b566fac3c6363f10ad861106e
Luc1Fer <atharv.shinde1309@gmail.com>
2026-07-30 19:18:13 +0530

    Add files via upload

---
+++ Codes/Four_Digit_Number.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Four_Digit_Number {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the Number:");
+        int x= sc.nextInt();
+        if(x>999 && x<10000){
+            System.out.println("The Number is 4 digit number");
+        }
+        else{
+            System.out.println("The Number is not a 4 digit number");
+        }
+    }
+}
---
+++ Codes/Greatest_Of_Three.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Greatest_Of_Three {
+    public static void main(String args[]) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the First Number:");
+        int x= sc.nextInt();
+        System.out.print("Enter the Second Number:");
+        int y= sc.nextInt();
+        System.out.print("Enter the Third Number:");
+        int z= sc.nextInt();
+        if(x>=y && x>=z){
+            System.out.println("The First Number is the greatest" + " " + x);
+        }
+        else if(y>=x && y>=z){
+            System.out.println("The Second Number is the greatest" + " " + y);
+        }
+        else{
+            System.out.println("The Third Number is the greatest" + " " + z);
+        }
+    }
+}

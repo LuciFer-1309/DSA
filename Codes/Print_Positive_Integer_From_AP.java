2173ce81d476597e02bffb7ad53afdd183753f19
Luc1Fer <atharv.shinde1309@gmail.com>
2026-08-01 19:50:23 +0530

    Add files via upload

---
+++ Codes/Print_Positive_Integer_From_AP.java
@@ -0,0 +1,7 @@
+public class Print_Positive_Integer_From_AP {
+    public static void main(String args[]) {
+        for (int i = 99; i >= 0; i -= 4) {
+            System.out.print(i + " ");
+        }
+    }
+}

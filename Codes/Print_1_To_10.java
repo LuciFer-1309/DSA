95168fb06135059cab423e9e31546d6f4ca8c011
Luc1Fer <atharv.shinde1309@gmail.com>
2026-08-18 20:01:19 +0530

    Delete Codes/Print_1_To_10.java

--- Codes/Print_1_To_10.java
+++
@@ -1,7 +0,0 @@
-public class Print_1_To_10 {
-    public static void main(String args[]){
-        for(int i=1; i<=10; i++){
-            System.out.println("The Number is" + " " + i);
-        }
-    }
-}

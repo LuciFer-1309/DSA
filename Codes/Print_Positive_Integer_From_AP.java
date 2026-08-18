4e3b5900c3f65bf8549a987fa99ab50b03cfb770
Luc1Fer <atharv.shinde1309@gmail.com>
2026-08-18 20:02:43 +0530

    Delete Codes/Print_Positive_Integer_From_AP.java

--- Codes/Print_Positive_Integer_From_AP.java
+++
@@ -1,7 +0,0 @@
-public class Print_Positive_Integer_From_AP {
-    public static void main(String args[]) {
-        for (int i = 99; i >= 0; i -= 4) {
-            System.out.print(i + " ");
-        }
-    }
-}

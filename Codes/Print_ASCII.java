d2a0c2548b132d3c24e8d5fe5b1b4ec11cd0b8f3
Luc1Fer <atharv.shinde1309@gmail.com>
2026-08-18 20:01:39 +0530

    Delete Codes/Print_ASCII.java

--- Codes/Print_ASCII.java
+++
@@ -1,7 +0,0 @@
-public class Print_ASCII {
-    public static void main(String args[]) {
-        for (int i = 65; i <= 90; i++) {
-            System.out.println((char) i + " " + i);
-        }
-    }
-}

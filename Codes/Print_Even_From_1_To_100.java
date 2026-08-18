e9365717157bddac4e6d9b41bb1d01531e6b4195
Luc1Fer <atharv.shinde1309@gmail.com>
2026-08-18 20:01:49 +0530

    Delete Codes/Print_Even_From_1_To_100.java

--- Codes/Print_Even_From_1_To_100.java
+++
@@ -1,9 +0,0 @@
-public class Print_Even_From_1_To_100 {
-    public static void main(String args[]){
-        for(int i=1; i<=100; i++){
-            if(i%2 == 0){
-                System.out.println(i);
-            }
-        }
-    }
-}
